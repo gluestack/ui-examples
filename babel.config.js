ea7f57b0f1f4219f099e24c78777d48755933ae4
Viraj-10 <virajjoshi132@gmail.com>
2023-06-05 13:09:39 +0530

    fix: dev env for performance

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,16 @@
-const myBabel = require('@gluestack-style/babel-plugin-styled-resolver');
-const path = require('path');
+const myBabel = require("@gluestack-style/babel-plugin-styled-resolver");
+const path = require("path");
 
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
+      ["transform-remove-console"],
       [
         myBabel,
         {
-          configPath: path.join(__dirname, './gluestack-ui.config.ts'),
+          configPath: path.join(__dirname, "./gluestack-ui.config.ts"),
         },
       ],
     ],
